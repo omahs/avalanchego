74e5c71050f5d66b5f2078e3922f9bab9df5b9d6
Maru Newby <maru.newby@avalabs.org>
2024-06-29 08:15:09 +0200

    bootstrap restart

---
+++ tests/bootstrap/e2e/e2e_test.go
@@ -0,0 +1,6 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package e2e
+
+// start network, make it available, get node id and ip
